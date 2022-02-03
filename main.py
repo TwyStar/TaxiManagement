184dfbb1c05e2e8e3076079147467f840a8958f0
TwyStar <98186484+TwyStar@users.noreply.github.com>
2022-02-03 14:44:35 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -12,24 +12,26 @@ curs = conn.cursor()
 
 
 #table creat
-try:
-    curs.execute("CREATE TABLE employee (name TEXT , education TEXT)")
-except sqlite3.OperationalError as e:
-    pass
-try:
-    curs.execute("CREATE TABLE cars (brand TEXT , platenumber TEXT , vintage INTEGER)")
-except sqlite3.OperationalError as e:
-    pass
-try:
-    curs.execute("CREATE TABLE inout (be DATE , ki DATE)")
-except sqlite3.OperationalError as e:
-    pass
+# try:
+#     curs.execute("CREATE TABLE employee (name TEXT , education TEXT)")
+# except sqlite3.OperationalError as e:
+#     pass
+# try:
+#     curs.execute("CREATE TABLE cars (brand TEXT , platenumber TEXT , vintage INTEGER)")
+# except sqlite3.OperationalError as e:
+#     pass
+# try:
+#     curs.execute("CREATE TABLE inout (be DATE , ki DATE)")
+# except sqlite3.OperationalError as e:
+#     pass
 
 
-#curs.execute("INSERT INTO employee VALUES ('jozsi','nemcigany')")
+
+#curs = conn.close()
+# conn = sqlite3.connect("database.db")
+# curs = conn.cursor()
 
 
-curs = conn.close()
 
 
 root = tk.Tk()
@@ -38,10 +40,10 @@ root.geometry("1920x1080")
 
 inp = ""
 inp2 = ""
+var_a = "jozsi"
+var_b = "jozsi2"
 clicked = False
 time = datetime.date.today()
-
-
 
 
 def printInput():
@@ -50,7 +52,17 @@ def printInput():
     lbl.config(text="Provided Input: "+inp + " " + inp2)
     conn = sqlite3.connect("database.db")
     curs = conn.cursor()
-    curs.execute("INSERT INTO employee VALUES ('Jozsi','nemcigany')")
+    print(inp, inp2)
+    curs.execute("INSERT INTO employee(name, education) VALUES (inp,inp2")
+    conn.commit()
+
+
+def insert_record(name, education):
+    curs.execute('''INSERT INTO employee (name,education) VALUES(?,?)''', (name, education))
+    conn.commit()
+
+insert_record(var_a,var_b)
+
 
 lbl = tk.Label(root, text="")
 e = tk.Text(root, height=5, width=20)
@@ -68,6 +80,7 @@ button.pack()
 #lbl = tk.Label(root, text="")
 lbl.pack()
 conn.close()
+
 
 
 root.mainloop()
